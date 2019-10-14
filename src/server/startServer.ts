c35f6c2cde82d9d30a3476f81cf0febe531a9112
Paul Kretschel <paul@kretschel.media>
2019-10-14 18:14:24 +0200

    Fix definition of dist directory

--- src/server/startServer.ts
+++ src/server/startServer.ts
@@ -2,9 +2,9 @@ import path from "path"
 import WebpackDevServer from "webpack-dev-server"
 import { runWebpack } from "./runWebpack"
 
-export function startServer(entry: string, port: number = 3000) {
-  const dist = path.resolve(__dirname, "../../dist")
+const dist = path.resolve(__dirname, "../../dist")
 
+export function startServer(entry: string, port: number = 3000) {
   const server = new WebpackDevServer(runWebpack(entry, dist), {
     contentBase: dist,
     port,
